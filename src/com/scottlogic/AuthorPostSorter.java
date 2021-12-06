0ecce86d588e063060fb89590e3d996ecf66bb33
gpellizzari-scottlogic <gpellizzari@scottlogic.com>
2021-12-06 09:50:37 +0000

    first commit

---
+++ src/com/scottlogic/AuthorPostSorter.java
@@ -0,0 +1,10 @@
+package com.scottlogic;
+
+import java.util.List;
+
+public class AuthorPostSorter implements PostSorter{
+    @Override
+    public List<UserPost> sort(List<UserPost> inputList) {
+        return null;
+    }
+}
