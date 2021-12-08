c7c49763de8a744883e11ce33e1db73f2e6c60c3
gpellizzari-scottlogic <gpellizzari@scottlogic.com>
2021-12-08 11:56:36 +0000

    removed a space

--- src/main/java/com/scottlogic/LikePostFilter.java
+++ src/main/java/com/scottlogic/LikePostFilter.java
@@ -29,7 +29,6 @@ public class LikePostFilter implements PostFilter {
                     filteredList.add(userPost);
                 }
             }
-
         }
         return filteredList;
     }
