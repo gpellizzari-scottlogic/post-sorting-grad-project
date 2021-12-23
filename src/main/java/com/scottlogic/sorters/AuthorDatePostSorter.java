207c03c91d5d71c984680a8282a8899fb8389c50
gpellizzari-scottlogic <gpellizzari@scottlogic.com>
2021-12-23 15:37:51 +0000

    Updated AuthorDatePostSorter to use Java Streams

--- src/main/java/com/scottlogic/sorters/AuthorDatePostSorter.java
+++ src/main/java/com/scottlogic/sorters/AuthorDatePostSorter.java
@@ -7,28 +7,25 @@ import com.scottlogic.filters.AuthorPostFilter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AuthorDatePostSorter implements PostSorter {
 
     @Override
     public List<UserPost> sort(List<UserPost> inputList, SortOrder sortOrder) {
-        List<UserPost> sortedList = null;
 
         if (inputList == null || inputList.isEmpty()) {
             return new ArrayList<UserPost>();
         }
 
-        List<UserPost> listSortedByAuthor = new AuthorPostSorter().sort(inputList, sortOrder);
-        String previousName = null;
-
-        for (UserPost userPost : listSortedByAuthor) {
-            if (!userPost.getAuthor().equals(previousName)) {
-                List<UserPost> tempList = new AuthorPostFilter(userPost.getAuthor()).filter(listSortedByAuthor);
-                sortedList.addAll(new DatePostSorter().sort(tempList, SortOrder.ASC));
-                previousName = userPost.getAuthor();
-            }
-        }
-        return sortedList;
+        return sortOrder == SortOrder.ASC
+                ? inputList.stream()
+                    .sorted(Comparator.comparing(UserPost::getAuthorSurname).thenComparing(UserPost::getDateTime))
+                    .collect(Collectors.toList())
+                : inputList.stream()
+                    .sorted(Comparator.comparing(UserPost::getAuthorSurname).reversed().thenComparing(UserPost::getDateTime))
+                    .collect(Collectors.toList());
     }
 }
